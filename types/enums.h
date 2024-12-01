b385c90d5c3dd441f028eb2de1f2714895739003
Kamil <pietrzak.kamil99@gmail.com>
2024-12-01 09:22:36 +0100

    add: enums for validating commands

---
+++ types/enums.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+enum class MetaCommandResutls {
+	SUCCESS,
+	UNRECOGNIZIBLE_COMMAND
+};
+
+enum class StatementResult {
+	SUCCESS,
+	UNRECOGNIZABLE_COMMAND
+};
+
+enum class StatementType {
+	INSERT,
+	UPDATE
+};
+
+#endif TYPES_H
