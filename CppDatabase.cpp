c647d17a2939e2c34498b70b4297a0fb9679c0ad
acrusedev <nominos09@gmail.com>
2024-12-16 19:29:29 +0100

    refactor: move checking for an empty buffer logic to main process

--- CppDatabase.cpp
+++ CppDatabase.cpp
@@ -6,19 +6,24 @@ auto main() -> int {
     const auto input_buffer = std::make_unique<InputBuffer>();
 
     while (true) {
+        std::cout << std::endl;
         InputBuffer::print_welcome_message();
         input_buffer->read_input();
 
+        if (input_buffer->is_input_empty()) {
+            std::cout << "Input buffer is empty. Please enter a valid command." << std::flush;
+            continue;
+        }
+
         if (input_buffer->get_buffer_first_char() == '.') {
             switch (exec_meta_command(input_buffer)) {
-            case MetaCommandResults::SUCCESS:
-                continue;
-            case MetaCommandResults::UNRECOGNIZED_COMMAND:
-                std::cout << "Unrecognizable META command " << input_buffer->get_buffer() << "\n";
+                case MetaCommandResults::SUCCESS:
+                    continue;
+                case MetaCommandResults::UNRECOGNIZED_COMMAND:
+                    std::cout << "Unrecognizable META command: " << input_buffer->get_buffer() << std::endl;
                 break;
             }
-        }
-        else {
+        } else {
             std::cout << "Unrecognizable QUERY command: '" << input_buffer->get_buffer() << "'" << std::endl;
         }
     }
